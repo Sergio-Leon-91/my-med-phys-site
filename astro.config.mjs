386fea7cfcc6c97f2c773c006ae8050dda868f41
Sergio-Leon-91 <sergio.leon9107@gmail.com>
2026-01-21 16:40:38 -0500

    Deploy Astro to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://Sergio-Leon-91.github.io",
+  base: "/my-med-phys-site/",
+});
